c1da538b8f8d4c2d391c0e79de69ff5dd3620277
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-10-13 16:41:10 +0900

    수정 기능 validation check

--- todoList/todoList.js
+++ todoList/todoList.js
@@ -107,7 +107,14 @@ addData = () => {
  * 단건 삭제
  */
 deleteData = (id) => {
-  document.getElementById("tr-" + id).remove();
+  let oldData = getAllData();
+  let newData = oldData.filter((item) => item.rowId != id);
+
+  document.getElementById("table-body").innerHTML = "";
+
+  for (let item of newData) {
+    appendRow(item);
+  }
 };
 
 /**
@@ -119,16 +126,28 @@ deleteSelectedData = () => {
     return;
   }
 
+  let oldData = getAllData();
+
   //체크된 체크박스를 가져온다.
   let checkbox = $("input:checkbox[name=checkRow]:checked");
 
   //체크된 체크박스의 값을 반복해 불러온다.
   checkbox.each(function (k, v) {
     let item = v.parentElement.parentElement;
-    $(item).remove();
 
-    $("input:checkbox[name='allCheck']").prop("checked", false);
+    let newData = oldData.filter((item) => item.rowId == id);
+    console.log("item");
+    console.log(item);
+    data.splice(item.rowId, 1);
   });
+
+  document.getElementById("table-body").innerHTML = "";
+
+  for (let item of oldData) {
+    appendRow(item);
+  }
+
+  $("input:checkbox[name='allCheck']").prop("checked", false);
 };
 
 /**
@@ -139,15 +158,11 @@ updateData = () => {
   let tableId = document.getElementById("table-id").value;
 
   if (afterText == "") {
-    alert("수정하실 텍스트를 입력하세요.");
+    alert("수정하실 문자열을 입력하세요.");
     return;
   }
 
   document.getElementById("content-" + tableId).textContent = afterText;
-
-  // 입력폼 초기화
-  document.getElementById("before-update-text").value = "";
-  document.getElementById("after-update-text").value = "";
 
   // 모달창 닫기
   document.getElementById("close-update-modal").click();
@@ -158,13 +173,19 @@ updateData = () => {
  */
 updateDataAll = () => {
   let data = getAllData();
-  let beforeText = document.getElementById("before-update-texts").value;
-  let afterText = document.getElementById("after-update-texts").value;
+  let beforeText = document.getElementById("before-update-text").value;
+  let afterText = document.getElementById("after-update-text").value;
+
+  let isUpdated = false;
+
+  if (afterText == "" || beforeText == "") {
+    alert("텍스트를 입력하세요.");
+    return;
+  }
 
   for (let item of data) {
     let oldStr = item.content;
     let newStr = "";
-    // oldStr = item.content;
 
     if (oldStr.indexOf(beforeText) != -1) {
       let arry = oldStr.split(beforeText);
@@ -172,22 +193,24 @@ updateDataAll = () => {
         newStr += arry[i];
         if (i != arry.length - 1) {
           newStr += afterText;
+          isUpdated = true;
         }
       }
       item.content = newStr;
     }
   }
 
+  if (!isUpdated) {
+    alert("찾으시는 문자열이 없습니다.");
+    return;
+  }
+
   for (let item of data) {
     document.getElementById("content-" + item.rowId).textContent = item.content;
   }
 
-  // 입력폼 초기화
-  document.getElementById("before-update-texts").value = "";
-  document.getElementById("after-update-texts").value = "";
-
   // 모달창 닫기
-  document.getElementById("close-modal").click();
+  document.getElementById("close-update-modal").click();
 };
 
 /**
@@ -206,7 +229,6 @@ getAllData = () => {
       .getElementById("table-body")
       .getElementsByTagName("tr")
       [i].getAttribute("rowId");
-    console.log("id : " + id);
 
     let rowData = {
       rowId: id,
@@ -250,7 +272,6 @@ getJsonFile = () => {
       type: "GET",
       dataType: "json",
       success: function (data) {
-        alert("Json 파일 받아오기");
         resolve(data);
       },
       error: function (error) {
@@ -283,15 +304,6 @@ promiseThen = () => {
 };
 
 /**
- * 수정 전 텍스트 화면에 세팅
- */
-setBeforeText = (id) => {
-  let text = document.getElementById("content-" + id).textContent;
-  document.getElementById("before-update-text").value = text;
-  document.getElementById("table-id").value = id;
-};
-
-/**
  * 테이블 행 추가
  */
 appendRow = (rowData) => {
@@ -300,6 +312,7 @@ appendRow = (rowData) => {
               <input
                 type="checkbox"
                 name="checkRow"
+                id="checkbox-${rowData.rowId}"
                 class="check_all_list"
                 onclick="checkAllList(event)"
               />
@@ -316,10 +329,49 @@ appendRow = (rowData) => {
               id="row-id"
             />
             <td><span style="color: red; cursor:pointer;" onclick="javascript:deleteData(${rowData.rowId});">삭제</span>&nbsp;&nbsp;
-          <span style="color: blue; cursor:pointer;" data-toggle="modal" data-target="#singleUpdateModal" onclick="setBeforeText(${rowData.rowId})">
+          <span style="color: blue; cursor:pointer;" data-toggle="modal" data-target="#update-modal" onclick="clickUpdateBtn(${rowData.rowId})">
     수정
   </span></td>
           </tr>`;
 
   document.getElementById("table-body").innerHTML += addHtml;
+};
+
+/**
+ * 단건 수정폼 세팅
+ */
+clickUpdateBtn = (id) => {
+  initUpdateForm();
+
+  let text = document.getElementById("content-" + id).textContent;
+
+  document.getElementById("update-btn").style.display = "block";
+  document.getElementById("update-all-btn").style.display = "none";
+  document.getElementById("before-update-text").value = text;
+  document.getElementById("table-id").value = id;
+
+  document.getElementById("before-update-text").disabled = true;
+};
+
+/**
+ * 일괄 수정폼 세팅
+ */
+clickUpdateAllbtn = () => {
+  initUpdateForm();
+
+  document.getElementById("before-update-text").disabled = false;
+  document.getElementById("update-all-btn").style.display = "block";
+  document.getElementById("update-btn").style.display = "none";
+};
+
+/**
+ * 수정폼 초기화
+ */
+initUpdateForm = () => {
+  document.getElementById("before-update-text").value = "";
+  document.getElementById("after-update-text").value = "";
+};
+
+initData = () => {
+  document.getElementById("table-body").innerHTML = "";
 };
